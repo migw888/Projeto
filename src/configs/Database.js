e60f5bd8df09928b84dc2202ffc046d0883a02f4
Daniel Pires <daniel20.pires@hotmail.com>
2026-07-11 10:10:45 -0300

    Correção na variavel de conexão ao banco

--- src/configs/Database.js
+++ src/configs/Database.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE,
+	database: process.env.DB_NAME,
     port: process.env.PORT,
 	waitForConnections: true,
 	connectionLimit: 10,
